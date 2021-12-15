a61fc127a8db888724750fc530532c1ec6387e9e
Pavel Lebedev <pashoktver90@gmail.com>
2021-12-15 10:46:42 +0300

    Updates

---
+++ precache-manifest.1e7340efa43c02577d50cc3318c9084c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cf7a0ed251f58ca491340ddcbfa818",
+    "url": "/IgnatHomeTasks/index.html"
+  },
+  {
+    "revision": "00c492c690b47f8b45cc",
+    "url": "/IgnatHomeTasks/static/css/main.9f5db95d.chunk.css"
+  },
+  {
+    "revision": "817764e6222d155e9abe",
+    "url": "/IgnatHomeTasks/static/js/2.7da38df7.chunk.js"
+  },
+  {
+    "revision": "df46f2766e65d61edf5f90b9c5080777",
+    "url": "/IgnatHomeTasks/static/js/2.7da38df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c492c690b47f8b45cc",
+    "url": "/IgnatHomeTasks/static/js/main.ef9cb065.chunk.js"
+  },
+  {
+    "revision": "a31ca8612372043d0078",
+    "url": "/IgnatHomeTasks/static/js/runtime-main.42c58730.js"
+  }
+]);
