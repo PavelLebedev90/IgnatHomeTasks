fba85c61ca119873c928e64d0379493ef8646c9e
Pavel Lebedev <pashoktver90@gmail.com>
2021-12-17 19:52:19 +0300

    HW6 done

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from '../../p1-main/m1-ui/u1-app/App.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -11,12 +12,13 @@ function HW6() {
     }
     const restore = () => {
         // setValue()
+        setValue(restoreState<string>('editable-span-value', value))
     }
 
     return (
-        <div>
+        <div className={s.wrapper}>
             <hr/>
-            homeworks 6
+            <div>homeworks 6</div>
 
             {/*should work (должно работать)*/}
             <div>
@@ -26,8 +28,10 @@ function HW6() {
                     spanProps={{children: value ? undefined : 'enter text...'}}
                 />
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+            <div>
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
